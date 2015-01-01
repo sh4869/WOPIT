1179af42b5528ea5e21cd95108dde29ddf38133d
sh4869 <nobuk4869@gmail.com>
2015-01-01 22:45:49 +0900

    Create DocomoOauthActivity

---
+++ app/src/main/java/android/sh4869/wopit/DocomoOauthActivity.java
@@ -0,0 +1,16 @@
+package android.sh4869.wopit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class DocomoOauthActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_docomo_oauth);
+    }
+
+}
