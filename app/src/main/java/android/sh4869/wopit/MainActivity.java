1ee720ba3fed5fedcb2776df00a12b00c7c4407f
sh4869 <nobuk4869@gmail.com>
2015-01-02 19:56:24 +0900

    Add Docomo image recognition sdk.

--- app/src/main/java/android/sh4869/wopit/MainActivity.java
+++ app/src/main/java/android/sh4869/wopit/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
@@ -37,5 +38,8 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void take_start(View view){
+
+    }
 
 }
